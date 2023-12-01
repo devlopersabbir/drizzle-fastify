23734dc0266e227949684f96bf47ef2a9b193906
devlopersabbir <devlopersabbir@gmail.com>
2023-12-01 20:04:49 +0600

    application schema

---
+++ src/schemas/app/application.ts
@@ -0,0 +1,9 @@
+import { pgTable, serial, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const applications = pgTable("applications", {
+  id: serial("id").primaryKey(),
+  uuid: uuid("uuid").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
