6a0b0fddc9ba9ac0ebaf15231756d558a3e92da3
devlopersabbir <devlopersabbir@gmail.com>
2023-12-05 19:05:40 +0600

    [feat] 🐢 User Schema

--- src/schema/schemas.ts
+++ src/schema/schemas.ts
@@ -1,4 +1,12 @@
-import { pgTable, serial, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  primaryKey,
+  serial,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const applications = pgTable("applications", {
   id: serial("id"),
@@ -8,4 +16,22 @@ export const applications = pgTable("applications", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
-export const users = pgTable("users", {});
+export const users = pgTable(
+  "users",
+  {
+    id: serial("id"),
+    uuid: uuid("uuid").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 256 }),
+    email: varchar("email", { length: 256 }).notNull().unique(),
+    applicationId: uuid("applicationId").references(() => applications.uuid),
+    password: varchar("password", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      cpk: primaryKey(users.email, users.applicationId),
+      idIndex: uniqueIndex("user_id_index").on(users.uuid),
+    };
+  }
+);
